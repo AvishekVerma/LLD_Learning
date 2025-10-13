a8cff837b77903112ad3ae8974043a03c77cbc9e
avi_01 <avishekverma298@gmail.com>
2025-10-13 08:47:44 +0530

    Completed Runnable

--- src/multiThreading/Client.java
+++ src/multiThreading/Client.java
@@ -7,6 +7,18 @@ public class Client {
         Thread t = new Thread(hwp);
         t.start();
 //        Thread.sleep(6000);
+        System.out.println("Main method line thread : " + Thread.currentThread().getName());
         System.out.println("Hello World form main method");
+
+        Adder adder = new Adder(10, 20);
+        Thread t1 = new Thread(adder);
+        t1.start();
+
+        // create 50 threads and for each thread print the square of the number 1 -> 50
+        for(int i=1;i<=50;i++){
+            SquarePrinter sq = new SquarePrinter(i);
+            Thread s = new Thread(sq);
+            s.start();
+        }
     }
 }
