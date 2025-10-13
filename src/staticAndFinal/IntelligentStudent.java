84ecb1933cd01c1f96805317eebb56cc185bcff3
avi_01 <avishekverma298@gmail.com>
2025-10-13 08:39:20 +0530

    Completed static and Final

---
+++ src/staticAndFinal/IntelligentStudent.java
@@ -0,0 +1,13 @@
+package staticAndFinal;
+
+public class IntelligentStudent extends Student {
+
+    public IntelligentStudent(int id){
+        super(id);
+    }
+
+    // The overridden method is final so, we can't override the method
+//    public void attendInterview(){
+//
+//    }
+}
