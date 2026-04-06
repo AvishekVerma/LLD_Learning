156f2dbc1de02fdaebb461ab24dc47507c6431e3
avi_01 <avishekverma298@gmail.com>
2026-04-06 20:54:53 +0530

    super keyword use cases

---
+++ src/DBConnection.java
@@ -0,0 +1,13 @@
+public class DBConnection {
+    private static DBConnection db = null;
+
+    private DBConnection(){
+
+    }
+
+    private static DBConnection createInstance(){
+        if(db==null)
+            db=new DBConnection();
+        return db;
+    }
+}
