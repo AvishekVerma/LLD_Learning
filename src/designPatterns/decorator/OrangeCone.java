db3f4f051b0379e0ab44127ada4a9792c4b640aa
avi_01 <avishekverma298@gmail.com>
2026-04-12 13:14:44 +0530

    decorator design pattern implementation.

---
+++ src/designPatterns/decorator/OrangeCone.java
@@ -0,0 +1,19 @@
+package designPatterns.decorator;
+
+public class OrangeCone implements IceCreamConeConstituents{
+
+
+    @Override
+    public int getCost() {
+        return 20;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Orange Cone";
+    }
+
+    public void setVitaminCLevel(int newLevel){
+
+    }
+}
