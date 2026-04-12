f3139abb7cecab2c5819ebf987cc8b6f5afbfe79
avi_01 <avishekverma298@gmail.com>
2026-04-12 17:43:57 +0530

    observer design pattern implementation.

---
+++ src/designPatterns/observer/InvoiceGenerator.java
@@ -0,0 +1,19 @@
+package designPatterns.observer;
+
+public class InvoiceGenerator implements OrderPlacedSubscriber{
+
+    public InvoiceGenerator(){
+        Flipkart flipkart = Flipkart.getInstance();
+        flipkart.registerSubscriber(this);
+    }
+
+    @Override
+    public ReturnData announceOrderPlaced() {
+        System.out.println("Generating Invoice");
+        return null;
+    }
+
+    public void generateInvoice(){
+        System.out.println("Generating Invoice");
+    }
+}
